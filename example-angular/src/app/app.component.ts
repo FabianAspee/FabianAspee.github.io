8538f660069424af90a271c2332389c6ee83ff62
faspeeencina <faspeeencina@gmail.com>
2021-08-04 22:11:24 +0200

    ok

--- example-angular/src/app/app.component.ts
+++ example-angular/src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ConfigService } from './config/config.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers: [ConfigService],
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  nameTurbine:string = "";
+  heroes: string[] = [];
+  editHero: string | undefined; // the hero currently being edited
+
+  constructor(private configService: ConfigService) {}
   title = 'example-angular';
   rest_get_call ="Turbine by Id";
   rest_get_call_list ="All Turbine";
@@ -14,8 +20,12 @@ export class AppComponent {
   rabbit_call = "Real Time Name Turbine";
   place_holder_result = "result of request";
   place_holder_input = "id turbine";
-  onInit(){
-    let myCompOneObj = new ConfigService(); 
-    myCompOneObj.myFunctionOne();
+
+  ngOnInit(){ 
+    
+  }
+  getTurbineByName(id:number): void {
+    this.configService.getNameTurbineById(id)
+      .subscribe(nameTurbine => (this.nameTurbine = nameTurbine));
   }
 }
